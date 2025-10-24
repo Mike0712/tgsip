1031c667672ac7e61e13578ef86bf493b39bb1b4
Mikhail Churikov <barrister2003@gmail.com>
2025-10-24 05:33:19 +0000

    add https server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // server.js
-const { createServer } = require('http');
+const { createServer: createHttpServer } = require('http');
+const { createServer: createHttpsServer } = require('https');
+const { readFileSync } = require('fs');
 const next = require('next');
 const { parse } = require('url');
 
@@ -11,13 +13,28 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = createServer((req, res) => {
+  const requestHandler = (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  });
+  };
+
+  let server;
+  
+  if (!dev && process.env.SSL_PATH_CERT && process.env.SSL_PATH_KEY) {
+    // Production with HTTPS
+    const httpsOptions = {
+      key: readFileSync(process.env.SSL_PATH_KEY),
+      cert: readFileSync(process.env.SSL_PATH_CERT)
+    };
+    server = createHttpsServer(httpsOptions, requestHandler);
+  } else {
+    // Development or production without SSL
+    server = createHttpServer(requestHandler);
+  }
 
   server.listen(port, hostname, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    const protocol = (!dev && process.env.SSL_PATH_CERT && process.env.SSL_PATH_KEY) ? 'https' : 'http';
+    console.log(`> Ready on ${protocol}://${hostname}:${port}`);
   });
 });
